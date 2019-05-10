34f706b8dcf2d207ff68a97e7702cd9ad870c06a
Evening <johnchangqi@hotmail.com>
2019-05-11 02:25:33 +0800

    Add blank inits for import referencing

---
+++ src/py/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 11 01:42:51 2019
+
+@author: johnc
+"""
+
---
+++ src/py/tests/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 11 01:43:05 2019
+
+@author: johnc
+"""
+
