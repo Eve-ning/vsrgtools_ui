32d90601f07f67ab57e5ebc5efd5c8ba7784836b
Evening <johnchangqi@hotmail.com>
2019-05-11 02:25:56 +0800

    Fix missing sort and arg

--- src/py/chart_column.py
+++ src/py/chart_column.py
@@ -13,8 +13,7 @@ class ChartColumn:
         '''Sorts the offset_list if _auto_reorder is True'''
         self._offset_list = np.sort(self._offset_list)
            
-    @classmethod
-    def _convert(offset_list):
+    def _convert(self, offset_list):
         '''Converts any input into valid offset_list then returns it'''
         # Map all into float
         offset_list = list(map(float, offset_list))
@@ -29,7 +28,6 @@ class ChartColumn:
         self._offset_list = self._convert(offset_list)
         
         self._sort()
-        self.offset_list()
         
     def __getitem__(self, key):
         return self._offset_list[key]
@@ -48,3 +46,5 @@ class ChartColumn:
     @offset_list.setter
     def offset_list(self, offset_list):
         self._offset_list = self._convert(offset_list)
+        self._sort()
+        
