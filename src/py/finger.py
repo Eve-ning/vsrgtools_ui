0b35702ccc678e9d7f49dc64cf7bc1d26e886104
Evening <johnchangqi@hotmail.com>
2019-05-12 01:08:47 +0800

    Create finger.py

---
+++ src/py/finger.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 12 00:38:03 2019
+
+@author: johnc
+"""
+
+class Finger:
+    '''Creates a simulated finger
+    
+    
+    '''
+    def __init__(self):
+        pass
