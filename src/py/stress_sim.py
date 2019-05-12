321914517960621d0ae9b87237af0dc8e2a01978
Evening <johnchangqi@hotmail.com>
2019-05-13 00:47:08 +0800

    Add framework for simulate function

--- src/py/stress_sim.py
+++ src/py/stress_sim.py
@@ -6,6 +6,7 @@ Created on Sun May 12 00:38:03 2019
 """
 
 from stress_model import StressModel as SM
+import pandas as pd
 
 class StressSim:
     '''Creates a simulation of stress
@@ -22,4 +23,23 @@ class StressSim:
             sm (StressModel): Import the Stress Model here.
         '''
         self.sm = sm
+        
+    def simulate(self,
+                 df,
+                 offsets_column_name = "offsets",
+                 events_column_name = "events") -> pd.DataFrame:
+        '''Simulates the StressModel and appends results to the df
+        
+        Args:
+            df (DataFrame): Import the DataFrame here.
+                The DataFrame must have the offsets and events column.
+            offsets_column_name (str): In the event that the column names are
+                not the same as the defaults, specify them here.
+            events_column_name (str): ^
+        '''
+        
+        pass
+        
+        
+        
       
