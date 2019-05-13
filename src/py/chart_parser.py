1a7bbb8c16954923335d9786a19748415a1ba4aa
Evening <johnchangqi@hotmail.com>
2019-05-13 15:05:24 +0800

    Update ChartParser with draft .osu parsing

--- src/py/chart_parser.py
+++ src/py/chart_parser.py
@@ -5,6 +5,8 @@ Created on Mon May 13 12:16:56 2019
 @author: johnc
 """
 
+import pandas as pd
+
 class ChartParser:
     '''Parses Charts from various formats
 
@@ -19,12 +21,53 @@ class ChartParser:
         self._parse_path()
         
     def _parse_path(self):
-        self.chart_format = self.chart_path.split(".")[1]
+        self._chart_format = self.chart_path.split(".")[1]
     
     def parse_osu(self):
         '''Parses osu file formats'''
-        pass
-    
+        f = open(self.chart_path, 'r')
+        
+        # Locate keys
+        keys = list(filter(lambda x: x.startswith('CircleSize:')))[0]
+        keys = int(keys.split(':')[1])
+        
+        # Lambda Function to calculate the actual column
+        clm = lambda value: round((int(value) * keys - 256) / 512)
+            
+        # Gets the chart and splits it into LNs and NNs
+        chart = list(filter(lambda x: x.count(':') > 4, f))
+        notes_l = list(filter(lambda x: x.count(':') == 4, chart))
+        lnotes_l = list(filter(lambda x: x.count(':') == 5, chart))
+        
+        # Convert the lines to values for the notes
+        # Values are in a tuple (Column, Offset)
+        notes = [(clm(x.split(',')[0]), # Gets column
+                        int(x.split(',')[2])) for # Gets offset
+                       x in notes_l]
+        
+        lnotes_h = [(clm(int(x.split(',')[0])), # Gets column
+                           int(x.split(',')[2])) for # Gets offset
+                          x in lnotes_l]
+        
+        lnotes_t = [(clm(int(x.split(',')[0])), # Gets column
+                           int(x.split(',')[5])) for # Gets offset
+                          x in lnotes_l]
+        
+        # Coerce to DataFrames
+        notes_df = pd.DataFrame(notes, columns = ['columns', 'offsets'])
+        notes_df['types'] = 'note'
+        
+        lnotes_h_df = pd.DataFrame(lnotes_h, columns = ['columns', 'offsets'])
+        lnotes_h_df['types'] = 'lnoteh'
+        
+        lnotes_t_df = pd.DataFrame(lnotes_t, columns = ['columns', 'offsets'])
+        lnotes_t_df['types'] = 'lnotet'
+        
+        # Join all together
+        chart_df = notes_df.append([lnotes_h_df, lnotes_t_df])
+        
+        return chart_df
+        
     def parse_sm(self):
         '''Parses sm file formats
         
@@ -41,7 +84,7 @@ class ChartParser:
         '''Automatically determines the correct parsing method from extension
         '''
         
-        f = self.chart_format
+        f = self._chart_format
         if (f == "osu"):
             return self.parse_osu()
         elif (f == "sm"):
