afb4f3cbf71f1b02fc5af6b4a8181a76161b8088
Evening <johnchangqi@hotmail.com>
2019-05-23 00:16:20 +0800

    Update with f.replay.parser

--- src/r/main.R
+++ src/r/main.R
@@ -1,16 +1,23 @@
+library(ggplot2)
+library(feather)
+library(reshape2)
+library(zoo)
+library(dplyr)
+library(magrittr)
+
 source("src/r/chart_parser.R")
 source("src/r/stress_sim.R")
+source("src/r/replay_parser.R")
 
 chart <- f.chart.parse("src/osu/tldne.osu")
+chart.rep <- f.replay.parse(chart, "src/feather/replay/3155787_tldne.feather")
 
 f.decay <- function(stress, duration){
   return(stress / 2 ** (duration / 1000))
 }
-
 f.spike <- function(stress, args){
   return((stress + args$adds) / args$mults)
 }
-
 df.mapping <-
   data.frame(types = c("note", "lnoteh", "lnotel"),
              adds  = c(50, 50, 20),
@@ -22,11 +29,4 @@ chart.sim <- f.stress.sim(chart = chart,
                           df.mapping = df.mapping,
                           stress.init = 0)
 
-library(ggplot2)
 
-ggplot(chart.sim) +
-  aes(x = offsets,
-      y = stress.decay) +
-  geom_smooth(aes(group = keys,
-                  color = keys),
-              span=0.3, se=F)
