e146d3a134cdba0ea0154f4f060703bc2f5e3f18
Evening <johnchangqi@hotmail.com>
2019-05-27 15:54:40 +0800

    Implement basic Broadcast Table format

--- src/r/broadcast_table.R
+++ src/r/broadcast_table.R
@@ -1,1 +1,18 @@
-bcst.match <- matrix(nrow = 7, ncol = 7)
+bcst.match <- matrix(nrow = 7, ncol = 7)keys = 7
+bcst.match <- data.frame(matrix(nrow = keys, ncol = keys))
+
+
+
+bcst.match[] = c(
+  
+  c(1,2,3,4,5,6,7),
+  c(1,2,3,4,5,6,7),
+  c(1,2,3,4,5,6,7),
+  c(1,2,3,4,5,6,7),
+  c(1,2,3,4,5,6,7),
+  c(1,2,3,4,5,6,7),
+  c(1,2,3,4,5,6,7)
+  
+)
+bcst.match
+
