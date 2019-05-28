b0a275fd9bb2af3099e26d0173bcece53ceb9e48
Evening <johnchangqi@hotmail.com>
2019-05-28 23:12:50 +0800

    Fix incorrect mapping issue
    Issue was that different hands weren't considered, so 1 to 7 means r.r. which indirectly means a ring jack, which isn't correct.
    
    This resolves that.

--- src/r/move_mapping.R
+++ src/r/move_mapping.R
@@ -1,6 +1,7 @@
 f.create.move.mapping <- function(keyset.select=NA,
                                   keyset=NA,
-                                  mapping=NA){
+                                  mapping=NA,
+                                  mapping.opp=NA){
   #' Creates the mapping to be used alongside with
   #' the broadcasted chart.
   #' 
@@ -49,6 +50,13 @@ f.create.move.mapping <- function(keyset.select=NA,
   #' c(3.5,3.0,5.0,1.0,1.3), # [M] mp,mr,mm,mi,mt
   #' c(2.7,1.2,1.2,5.0,2.4), # [I] ip,ir,im,ii,it
   #' c(2.7,1.7,1.5,1.9,5.0)  # [T] tp,tr,tm,ti,tt
+  #' 
+  #' @param mapping.opp A custom **5 by 5 matrix** to be used
+  #' to map onto actions that involve alternating hands.
+  #' If NA, a default would be used
+  #' 
+  #' Same format as `mapping`. However, pr would mean
+  #' FROM Hand 1 Pinky to Hand 2 Ring. Rest is the same idea.
 
   # Loads Mapping, if NA, it's defaulted
   f.load.mapping <- function(mapping){
@@ -61,11 +69,18 @@ f.create.move.mapping <- function(keyset.select=NA,
     #' Details of the format is located in the
     #' parent help
     
-    fngr <- data.frame(P = c(rep(0,5)),
-                       R = c(rep(0,5)),
-                       M = c(rep(0,5)),
-                       I = c(rep(0,5)),
-                       T = c(rep(0,5)))
+    fngr <- data.frame(P1 = c(rep(0,5)),
+                       R1 = c(rep(0,5)),
+                       M1 = c(rep(0,5)),
+                       I1 = c(rep(0,5)),
+                       T1 = c(rep(0,5)))
+    
+    fngr.opp <- data.frame(P2 = c(rep(0,5)),
+                           R2 = c(rep(0,5)),
+                           M2 = c(rep(0,5)),
+                           I2 = c(rep(0,5)),
+                           T2 = c(rep(0,5)))
+    
     
     # If not defined, we will load default
     if (is.na(mapping)){
@@ -78,40 +93,73 @@ f.create.move.mapping <- function(keyset.select=NA,
         c(2.7,1.7,1.5,1.9,5.0)  # [T] tp,tr,tm,ti,tt
       )
     } else {
-      fngr[] = mapping
+      fngr[] <- mapping
+    }
+    if (is.na(mapping.opp)){
+      fngr.opp[] <- c(
+        # [P] [R] [M] [I] [T]
+        c(1.9,1.8,1.7,1.6,1.5), # [P] pp,pr,pm,pi,pt
+        c(1.8,1.7,1.6,1.5,1.4), # [R] rp,rr,rm,ri,rt
+        c(1.7,1.6,1.5,1.4,1.3), # [M] mp,mr,mm,mi,mt
+        c(1.6,1.5,1.4,1.3,1.2), # [I] ip,ir,im,ii,it
+        c(1.5,1.4,1.3,1.2,1.1)  # [T] tp,tr,tm,ti,tt
+      )
+    } else {
+      fngr.opp[] = mapping.opp
     }
     
-    fngr$froms = c('P','R','M','I','T')
+    fngr$froms = c('P1','R1','M1','I1','T1')
+    fngr.opp$froms = c('P1','R1','M1','I1','T1')
     
     fngr %<>% 
       melt(variable.name = 'tos',
            value.name = 'moves')
+    fngr.opp %<>% 
+      melt(variable.name = 'tos',
+           value.name = 'moves')
+  
+    # FROM 1 TO 2 to 2 TO 1
+    fngr.opp.s <- fngr.opp
+    colnames(fngr.opp.s) <- c('tos', 'froms', 'moves')
+    
+    # FROM 1 TO 1 to 2 TO 2
+    fngr.s <- fngr
+    fngr.s$froms <- sub("[[:digit:]]", "2", fngr.s$froms)
+    fngr.s$tos <- sub("[[:digit:]]", "2", fngr.s$tos)
+    
+    fngr <- rbind(fngr, fngr.s, fngr.opp, fngr.opp.s)
+    
     return(fngr)
   }
   
-  # Chooses between keyset or keyset.select, if both
-  # NA, stop()
+  # Chooses between keyset or keyset.select, if both NA, stop()
   f.load.keyset <- function(keyset, keyset.select){
     if (!is.na(keyset)){
       return(keyset)
     } else if (!is.na(keyset.select)){
     move.keysets = list(
       '4' = data.frame(keys = 1:4,
-                       fingers = c('M','I','I','M')),
-      '5' = data.frame(keys = 1:5,
-                       fingers = c('M','I','T','I','M')),
+                       fingers = c('M1','I1','I2','M2')),
+      '5R' = data.frame(keys = 1:5,
+                        fingers = c('M1','I1','T2','I2','M2')),
+      '5L' = data.frame(keys = 1:5,
+                        fingers = c('M1','I1','T1','I2','M2')),
       '6' = data.frame(keys = 1:6,
-                       fingers = c('R','M','I','I','M','R')),
-      '7' = data.frame(keys = 1:7,
-                       fingers = c('R','M','I','T','I','M','R')),
+                       fingers = c('R1','M1','I1','I2','M2','R2')),
+      '7R' = data.frame(keys = 1:7,
+                        fingers = c('R1','M1','I1','T2','I2','M2','R2')),
+      '7L' = data.frame(keys = 1:7,
+                        fingers = c('R1','M1','I1','T1','I2','M2','R2')),
       '8SPL' = data.frame(keys = 1:8,
-                          fingers = c('P','R','M','I','T','I','M','R')),
+                          fingers = c('P1','R1','M1','I1','T2','I2','M2','R2')),
       '8SPR' = data.frame(keys = 1:8,
-                          fingers = c('R','M','I','T','I','M','R','P')),
+                          fingers = c('R1','M1','I1','T1','I2','M2','R2','P2')),
       '8SYM' = data.frame(keys = 1:8,
-                          fingers = c('R','M','I','T','T','I','M','R')),
-      '9' = data.frame(keys = 1:9,
-                       fingers = c('P','R','M','I','T','I','M','R','P'))
+                          fingers = c('R1','M1','I1','T1','T2','I2','M2','R2')),
+      '9R' = data.frame(keys = 1:9,
+                        fingers = c('P1','R1','M1','I1','T2','I2','M2','R2','P2')),
+      '9L' = data.frame(keys = 1:9,
+                        fingers = c('P1','R1','M1','I1','T1','I2','M2','R2','P2'))
     )
     return(move.keysets[[keyset.select]])
     } else {
