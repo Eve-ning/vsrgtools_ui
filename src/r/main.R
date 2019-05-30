32c39ba836a2b4b8b749a35a89924906223d1e82
Evening <johnchangqi@hotmail.com>
2019-05-30 12:31:52 +0800

    Group hyperparameters together with parents

--- src/r/main.R
+++ src/r/main.R
@@ -14,37 +14,47 @@ source("src/r/alpha_calc.R")
 
 # chart.rep <- f.replay.parse(chart, "src/feather/replay/3155787_tldne.feather")
 
-f.stress.decay <- function(stress, duration){
-  return(stress / 1.5 ** (duration / 10000))
-}
-f.stress.spike <- function(stress, args){
-  return((stress + args$adds) / args$mults)
-}
-f.alpha <- function(diffs, moves.values){
-  return(moves.values * (1/diffs))
-}
-stress.mapping <-
-  data.frame(types = c("note", "lnoteh", "lnotel"),
-             adds  = c(50, 50, 20),
-             mults = c(1,1,1))
+
+
+
 chart.path <- 'src/osu/aiae.osu'
-keyset.select <- '4'
+
 
 chart <- f.chart.parse(chart.path)
-chart.sim <- f.stress.sim(chart = chart,
-                          f.spike = f.stress.spike,
-                          f.decay = f.stress.decay,
-                          df.mapping = stress.mapping,
-                          stress.init = 0)
 
-# Stress is lost here
-# broadcast on diff
-chart.bcst <- f.diff.broadcast(chart.sim,
+# Stress Simulation
+{
+  f.stress.decay <- function(stress, duration){
+    return(stress / 1.5 ** (duration / 10000))
+  }
+  f.stress.spike <- function(stress, args){
+    return((stress + args$adds) / args$mults)
+  }
+  stress.mapping <-
+    data.frame(types = c("note", "lnoteh", "lnotel"),
+               adds  = c(50, 50, 20),
+               mults = c(1,1,1))
+  chart.sim <- f.stress.sim(chart = chart,
+                            f.spike = f.stress.spike,
+                            f.decay = f.stress.decay,
+                            df.mapping = stress.mapping,
+                            stress.init = 0)
+}
+
+# Difference Broadcasting
+chart.bcst <- f.diff.broadcast(chart,
                                ignore.types = c('lnotel'))
 
-move.mapping <- f.create.move.mapping(keyset.select)
-chart.alpha <- f.alpha.calc(chart.bcst, move.mapping, f.alpha)
-
+# Alpha Calculation
+{
+keyset.select <- '4'
+f.alpha <- function(diffs, moves.values){
+  return(moves.values * (1/diffs ** 2))
+}
+chart.alpha <- f.alpha.calc(chart.bcst,
+                            f.create.move.mapping(keyset.select),
+                            f.alpha)
+}
 
 require(ggdark) 
 
