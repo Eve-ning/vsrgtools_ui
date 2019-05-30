d53b775a2010dd62bebdcc23da8040d39c02d187
Evening <johnchangqi@hotmail.com>
2019-05-30 22:27:01 +0800

    Rename map to chart and implement ignore threshold

--- src/r/replay_parser.R
+++ src/r/replay_parser.R
@@ -1,13 +1,15 @@
-f.replay.parse <- function(map, replay.path){
+f.replay.parse <- function(chart, replay.path, ignore.threshold = 100){
   #' Parses the replay generated through the Python API downloader.
   #' 
   #' @description Using proximity matching, data of the replay is
-  #' joined with the map, so we can tell what notes are the replay
+  #' joined with the chart, so we can tell what notes are the replay
   #' related to.
   #' 
-  #' @param map A **data.frame** that needs to have types, offsets,
+  #' @param chart A **data.frame** that needs to have types, offsets,
   #' and keys
   #' @param replay.path **Path** of the replay generated.
+  #' @param ignore.threshold **Numeric** to indicate what deviations should
+  #' be ignored.
   #' 
   #' @return Returns a joined **data.frame** with replay.offsets
   #' on the following column.
@@ -15,40 +17,43 @@ f.replay.parse <- function(map, replay.path){
   require(dplyr)
   require(feather)
   
-  f.similarity.match <- function(map, replay){
-    #' Does a similarity match between map and replay
-    "Map should only come in keys, we will need to 
+  f.similarity.match <- function(chart, replay){
+    #' Does a similarity match between chart and replay
+    "chart should only come in keys, we will need to 
     transform it into actions, which will help in
     pairing"
-    map$actions <- map$keys
-    map$actions[map$types == 'lnotel'] <- -map$actions[map$types == 'lnotel']
-    map$replay.offsets <- NA
-    actions.unq <- unique(map$actions)
+    chart$actions <- chart$keys
+    chart$actions[chart$types == 'lnotel'] <- -chart$actions[chart$types == 'lnotel']
+    chart$replay.offsets <- NA
+    actions.unq <- unique(chart$actions)
     
-    map.ac.split <- split(map, f=map$actions)
+    chart.ac.split <- split(chart, f=chart$actions)
     replay.ac.split <- split(replay, f=replay$actions)
     
-    map.ac.list <- c()
+    chart.ac.list <- c()
     for (i in actions.unq) {
-      map.ac <- map.ac.split[[as.character(i)]]
+      chart.ac <- chart.ac.split[[as.character(i)]]
       replay.ac <- replay.ac.split[[as.character(i)]]
       
-      for (row in 1:nrow(map.ac)) {
+      for (row in 1:nrow(chart.ac)) {
         # From all offsets of the replay df, we will deduct a looped
-        # map df offset
+        # chart df offset
         # We will get the minimum (which is the closest match)
         # Then we throw it into df.joined
         replay.match <- replay.ac[which.min(abs(replay.ac$offsets -
-                                                map.ac$offsets[row]))[1],]
+                                                chart.ac$offsets[row]))[1],]
 
-        map.ac[row, 'replay.offsets'] <- replay.match$offsets
+        chart.ac[row, 'replay.offsets'] <- replay.match$offsets
       }
-      map.ac.list <- append(map.ac.list, list(map.ac))
+      chart.ac.list <- append(chart.ac.list, list(chart.ac))
     }
-    return(bind_rows(map.ac.list))
+    return(bind_rows(chart.ac.list))
   }
   
   replay <- read_feather(replay.path)
-  map <- f.similarity.match(map, replay)
-  return(map)
+  chart <- f.similarity.match(chart, replay)
+  
+  chart$devs <- abs(chart$offsets - chart$replay.offsets)
+  chart.rep <- subset(chart.rep, chart.rep$devs < ignore.threshold)
+  return(chart)
 }
