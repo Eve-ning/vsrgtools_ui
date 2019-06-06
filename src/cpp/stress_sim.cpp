48394c2b498053bea10d3091172db2c9ef26a3ee
Evening <johnchangqi@hotmail.com>
2019-06-07 07:15:08 +0800

    Create basic framework for Stress Sim convert

--- src/cpp/stress_sim.cpp
+++ src/cpp/stress_sim.cpp
@@ -1,23 +1,27 @@
 #include <Rcpp.h>
 using namespace Rcpp;
 
-// This is a simple example of exporting a C++ function to R. You can
-// source this function into an R session using the Rcpp::sourceCpp 
-// function (or via the Source button on the editor toolbar). Learn
-// more about Rcpp at:
-//
-//   http://www.rcpp.org/
-//   http://adv-r.had.co.nz/Rcpp.html
-//   http://gallery.rcpp.org/
-//
-
-// [[Rcpp::export]]
-NumericVector timesTwo(NumericVector x) {
-  return x * 2;
+// Note that separate keys should be passed in different instances
+// Can probably add a wrapper function to handle that
+// V1: Stress Value before Spike (Including Decay)
+// V2: Stress Value after Spike (Excluding Decay)
+List simulate_key(NumericVector offset_v,
+                  NumericVector key_v,
+                  List args,
+                  Function spike,
+                  Function decay,
+                  unsigned int keys,
+                  double stress = 0) {
+  
+  unsigned int rows = offset_v.length();
+  
+  NumericVector stress_decay(rows);
+  NumericVector stress_spike(rows);
+  NumericVector stress_trackers(keys);
+  
+  for (unsigned int row; row < rows; row ++) {
+    stress = spike();
+  }
+  
+  
 }
-
-
-// You can include R code blocks in C++ files processed with sourceCpp
-// (useful for testing and development). The R code will be automatically 
-// run after the compilation.
-//
