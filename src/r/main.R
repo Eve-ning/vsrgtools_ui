7f67b4237a5572c43c3b41a6d207bf93dd9b9f9b
Evening <johnchangqi@hotmail.com>
2019-06-12 16:30:12 +0800

    Move all library functions to separate repo

--- src/r/main.R
+++ src/r/main.R
@@ -5,29 +5,25 @@ require(dplyr)
 require(magrittr)
 require(microbenchmark)
 {
-source("src/r/chart_parser.R")
-source("src/r/stress_sim.R")
-source("src/r/replay_parser.R")
-source("src/r/diff_broadcast.R")
-source("src/r/create_move_mapping.R")
-source("src/r/alpha_calc.R")
+  library(osutools)
 }
+
 microbenchmark(
   "al" = {
   # Chart Parser
     chart.path <- 'src/osu/princbride.osu'
-    chart <- f.chart.parse(chart.path)
+    chart <- chart.parse(chart.path)
   
   # Replay Parser
-    chart.rep <- f.replay.parse(chart, "src/feather/replay/3155787_tldne.feather",
+    chart.rep <- replay.parse(chart, "src/feather/replay/3155787_tldne.feather",
                                 ignore.threshold = 100)
   
   # Stress Simulation
-    chart.sim <- f.stress.sim(chart = chart,
+    chart.sim <- stress.sim(chart = chart,
                               stress.init = 0)
   
   # Difference Broadcasting
-    chart.bcst <- f.diff.broadcast(chart,
+    chart.bcst <- diff.broadcast(chart,
                                    ignore.types = c('lnotel'))
   
   # Alpha
@@ -35,9 +31,9 @@ microbenchmark(
     f.alpha <- function(diffs, moves.values){
       return(moves.values * (1/diffs ** 2))
     }
-    chart.alpha <- f.alpha.calc(chart.bcst,
-                                suppressMessages(f.create.move.mapping(keyset.select)),
-                                f.alpha)
+    chart.alpha <- alpha.calc(chart.bcst,
+                              suppressMessages(f.create.move.mapping(keyset.select)),
+                              f.alpha)
   }
 , times = 1
 )
