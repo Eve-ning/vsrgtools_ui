40fdcb96631a4de45ed95bcaefd2ea1107e4c714
Evening <johnchangqi@hotmail.com>
2019-06-12 18:14:28 +0800

    Move main to example for other users' reference

--- src/r/main.R
+++ src/r/main.R
@@ -1,35 +1,3 @@
-require(microbenchmark)
-{
-  library(osutools)
-}
+library(osutools)
 
-microbenchmark(
-  "al" = {
-  # Chart Parser
-    chart.path <- "src/osu/princbride.osu"
-    chart <- chartParse(chart.path)
-  
-  # Replay Parser
-    chart.rep <- replayParse(chart,
-                             "src/feather/replay/3155787_tldne.feather",
-                             ignore.threshold = 100)
-  
-  # Stress Simulation
-    chart.sim <- stressSim(chart = chart,
-                              stress.init = 0)
-  
-  # Difference Broadcasting
-    chart.bcst <- diffBroadcast(chart,
-                                   ignore.types = c('lnotel'))
-  
-  # Alpha
-    keyset.select <- '4'
-    f.alpha <- function(diffs, moves.values){
-      return(moves.values * (1/diffs ** 2))
-    }
-    chart.alpha <- alphaCalc(chart.bcst,
-                             suppressMessages(f.create.move.mapping(keyset.select)),
-                             f.alpha)
-  }
-, times = 1
-)
+
