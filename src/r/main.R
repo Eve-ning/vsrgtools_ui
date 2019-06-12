2cd86f442f1eeae389df4bfeddb03aa5288e1659
Evening <johnchangqi@hotmail.com>
2019-06-12 17:58:29 +0800

    Update by Style changes of osutools

--- src/r/main.R
+++ src/r/main.R
@@ -6,19 +6,20 @@ require(microbenchmark)
 microbenchmark(
   "al" = {
   # Chart Parser
-    chart.path <- 'src/osu/princbride.osu'
-    chart <- chart.parse(chart.path)
+    chart.path <- "src/osu/princbride.osu"
+    chart <- chartParse(chart.path)
   
   # Replay Parser
-    chart.rep <- replay.parse(chart, "src/feather/replay/3155787_tldne.feather",
-                                ignore.threshold = 100)
+    chart.rep <- replayParse(chart,
+                             "src/feather/replay/3155787_tldne.feather",
+                             ignore.threshold = 100)
   
   # Stress Simulation
-    chart.sim <- stress.sim(chart = chart,
+    chart.sim <- stressSim(chart = chart,
                               stress.init = 0)
   
   # Difference Broadcasting
-    chart.bcst <- diff.broadcast(chart,
+    chart.bcst <- diffBroadcast(chart,
                                    ignore.types = c('lnotel'))
   
   # Alpha
@@ -26,9 +27,9 @@ microbenchmark(
     f.alpha <- function(diffs, moves.values){
       return(moves.values * (1/diffs ** 2))
     }
-    chart.alpha <- alpha.calc(chart.bcst,
-                              suppressMessages(f.create.move.mapping(keyset.select)),
-                              f.alpha)
+    chart.alpha <- alphaCalc(chart.bcst,
+                             suppressMessages(f.create.move.mapping(keyset.select)),
+                             f.alpha)
   }
 , times = 1
 )
