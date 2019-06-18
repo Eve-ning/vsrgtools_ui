1ba14de25b2979a13753a1b4dc5bdb2c88346295
Evening <johnchangqi@hotmail.com>
2019-06-19 00:35:39 +0800

    Convert UI to shinydashboard

--- server.R
+++ server.R
@@ -5,16 +5,19 @@ shinyServer(function(input, output) {
     source("r/parseInput.R")
 
     observeEvent(input$parse, {
-        osu <- strsplit(input$osu, '\n')
-        osu <- osu[[1]]
-        osu <- parseInput(osu,
-                          keyset.select = input$keyset.select,
-                          span = input$smoothing,
-                          rate = input$rate,
-                          dif.quant = input$dif.quant)
+        text <- strsplit(input$text, '\n')
+        text <- text[[1]]
+        chart <- parseInput(text,
+                            keyset.select = input$keyset.select,
+                            span = input$smoothing,
+                            rate = input$rate,
+                            dif.quant = input$dif.quant)
         
-        output$plt <- renderPlot(osu$plt, height="auto")
-        output$dly <- renderText(paste0(osu$dly, " seconds elapsed "))
+        output$plt <- renderPlot(chart$plt, height="auto")
+        output$dly <- renderValueBox(
+            valueBox(round(chart$dly, 2), subtitle = "seconds elapsed ",
+                     color = "maroon")
+            )
             
     })
 })
--- ui.R
+++ ui.R
@@ -1,22 +1,12 @@
-library(shiny)
+library(shinydashboard)
 
-# Define UI for application that draws a histogram
-shinyUI(fluidPage(
+dashboardPage(
 
-    # Application title
-    titlePanel("VSRG Simulator Interface"),
-
-    mainPanel(
-        
-        plotOutput("plt", height = "900px"),
-        textOutput("dly")
-        ),
-    
-    sidebarPanel(
-        textAreaInput("osu",
-                      "Input .osu contexts here",
+    dashboardHeader(title = "estear"),
+    dashboardSidebar(
+        textAreaInput("text",
+                      label = "Input file contents here",
                       placeholder = "<.osu file>"),
-        
         sliderInput("rate", "Rate", 0.1, 10.00, value = 1.0, step = 0.1),
         sliderInput("smoothing", "Smoothing", 0.01, 1.00, value = 0.1, step = 0.01),
         selectInput("keyset.select",
@@ -24,9 +14,54 @@ shinyUI(fluidPage(
                     choices = c("4", "5R", "5L", "6", "7R", "7L", "8SPR", "8SPL", "8SYM", "9R", "9L"),
                     selected = "4"),
         sliderInput("dif.quant", "Calculation Quantile", 0.01, 1.00, value = 0.85, step = 0.01),
-    
-        
         actionButton("parse", "Parse Input")
-    )
-    )
+        
+    ),
+    dashboardBody(
+        fluidRow(
+            column(width = 12,
+                box(
+                    title = "Plot Output",
+                    status = "info",
+                    width = NULL,
+                    background = "light-blue",
+                    plotOutput("plt", height = "900px")
+                ),
+                valueBoxOutput("dly")
+            )
+        )
+        )
+        
+        
+        
+        
+        
+        
+    
+    # # Application title
+    # titlePanel("VSRG Simulator Interface"),
+    # 
+    # mainPanel(
+    #     
+    #     plotOutput("plt", height = "900px"),
+    #     textOutput("dly")
+    #     ),
+    # 
+    # sidebarPanel(
+    #     textAreaInput("osu",
+    #                   "Input .osu contexts here",
+    #                   placeholder = "<.osu file>"),
+    #     
+    #     sliderInput("rate", "Rate", 0.1, 10.00, value = 1.0, step = 0.1),
+    #     sliderInput("smoothing", "Smoothing", 0.01, 1.00, value = 0.1, step = 0.01),
+    #     selectInput("keyset.select",
+    #                 label = "Keyset Selection",
+    #                 choices = c("4", "5R", "5L", "6", "7R", "7L", "8SPR", "8SPL", "8SYM", "9R", "9L"),
+    #                 selected = "4"),
+    #     sliderInput("dif.quant", "Calculation Quantile", 0.01, 1.00, value = 0.85, step = 0.01),
+    # 
+    #     
+    #     actionButton("parse", "Parse Input")
+    # )
+    # )
 )
