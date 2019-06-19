39439f00c3f6c6412eba04c53a02036269d1492e
Evening <johnchangqi@hotmail.com>
2019-06-20 02:24:51 +0800

    Add Value Boxes

--- server.R
+++ server.R
@@ -18,6 +18,35 @@ shinyServer(function(input, output) {
             valueBox(round(chart$dly, 5), subtitle = "seconds elapsed ",
                      color = "maroon")
             )
+        dif <- reactive(list("jck" = quantile(chart$s.mdls$jck$jack.invs,
+                                              input$dif.quant,
+                                              type = 7),
+                             "mtn" = quantile(chart$s.mdls$mtn$diffs.invs,
+                                              input$dif.quant,
+                                              type = 7),
+                             "dns" = quantile(chart$s.mdls$dns$counts[chart$s.mdls$dns$counts > 0],
+                                              input$dif.quant,
+                                              type = 7)))
+        
+        output$jck <- renderValueBox(
+            valueBox(round(dif()$jck, 5) * 1000,
+                     subtitle = paste0("Jack Quantile ", input$dif.quant * 100, "%"),
+                     color = "light-blue")
+        )
+        
+        output$mtn <- renderValueBox(
+            valueBox(round(dif()$mtn, 5) * 1000,
+                     subtitle = paste0("Motion Quantile ", input$dif.quant * 100, "%"),
+                     color = "blue")
+        )
+        output$dns <- renderValueBox(
+            valueBox(round(dif()$dns, 5),
+                     subtitle = paste0("Density Quantile ", input$dif.quant * 100, "%"),
+                     color = "navy")
+        )
             
     })
+
+    
+    
 })
