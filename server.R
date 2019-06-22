5e1730e26a998a48191beb4e2eee5455863bd479
Evening <johnchangqi@hotmail.com>
2019-06-22 18:37:47 +0800

    Update interface

--- server.R
+++ server.R
@@ -25,7 +25,9 @@ shinyServer(function(input, output) {
             mtn.jack.weight = input$mtn.jack,
             jck.pow = input$jck.pow,
             mtn.pow = input$mtn.pow,
-            dns.pow = input$dns.pow
+            dns.pow = input$dns.pow,
+            chart.rate = input$rate,
+            sim.bin.size = input$sim.bin.size 
         )
         end.time <- Sys.time()
         dly <- end.time - start.time
@@ -36,7 +38,7 @@ shinyServer(function(input, output) {
             geom_smooth(span = input$smoothing, method='loess', se=F) 
         
         chart.model.plt <- ggplot(chart$model) +
-            aes(offsets, values) +
+            aes(bins, values) +
             geom_smooth(span = input$smoothing, method='loess', se=F)
         
         output$stress.plt <- renderPlot(chart.stress.plt, height="auto")
@@ -44,61 +46,42 @@ shinyServer(function(input, output) {
 
         output$dly <- renderValueBox(valueBox(dly, subtitle = "ms delay", color = 'teal'))
         output$calc1 <- renderValueBox(
-            valueBox(quantile(chart$sim$stress, input$dif.quant), subtitle = " Stress",
+            valueBox(paste(
+                round(quantile(chart$sim$stress, input$dif.quant)[[1]],2),
+                round(quantile(chart$sim$stress, input$dif.quant)[[2]],2),
+                sep = ' - '
+                ), subtitle = " Stress",
                      color = "maroon")
         )
         output$calc2 <- renderValueBox(
-            valueBox(quantile(chart$model$values, input$dif.quant), subtitle = " Difficulty",
+            valueBox(paste(
+                round(quantile(chart$model$values, input$dif.quant)[[1]],2),
+                round(quantile(chart$model$values, input$dif.quant)[[2]],2),
+                sep = ' - '
+            ), subtitle = " Difficulty",
                      color = "maroon")
         )
-        # dif <- reactive(list("jck" = quantile(chart$s.mdls$jck$values,
-        #                                       input$dif.quant,
-        #                                       type = 7),
-        #                      "mtn" = quantile(chart$s.mdls$mtn$values / 100,
-        #                                       input$dif.quant,
-        #                                       type = 7),
-        #                      "dns" = quantile(chart$s.mdls$dns$values[chart$s.mdls$dns$values > 0],
-        #                                       input$dif.quant,
-        #                                       type = 7),
-        #                      "calc1" = quantile(chart$s.mdls$mtn$values *
-        #                                         chart$s.mdls$dns$values,
-        #                                         input$dif.quant,
-        #                                         type = 7),
-        #                      "calc2" = quantile(chart$s.mdls$mtn$values *
-        #                                         chart$s.mdls$jck$values *
-        #                                         chart$s.mdls$dns$values * 100,
-        #                                         input$dif.quant,
-        #                                         type = 7)))
-        # 
-        # output$jck <- renderValueBox(
-        #     valueBox(round(dif()$jck, 5) * 1000,
-        #              subtitle = paste0("Jack Quantile ", input$dif.quant * 100, "%"),
-        #              color = "light-blue")
-        # )
-        # 
-        # output$mtn <- renderValueBox(
-        #     valueBox(round(dif()$mtn, 5) * 1000,
-        #              subtitle = paste0("Motion Quantile ", input$dif.quant * 100, "%"),
-        #              color = "blue")
-        # )
-        # output$dns <- renderValueBox(
-        #     valueBox(round(dif()$dns, 5),
-        #              subtitle = paste0("Density Quantile ", input$dif.quant * 100, "%"),
-        #              color = "navy")
-        # )
-        # 
-        # output$calc1 <- renderValueBox(
-        #     valueBox(round(dif()$calc1, 5),
-        #              subtitle = paste0("Calculation VER 1 ", input$dif.quant * 100, "%"),
-        #              color = "red")
-        # )
-        # 
-        # output$calc2 <- renderValueBox(
-        #     valueBox(round(dif()$calc2, 5),
-        #              subtitle = paste0("Calculation VER 2 ", input$dif.quant * 100, "%"),
-        #              color = "yellow")
-        # )
-            
+        
+        output$params.log <- renderText(
+            paste(
+                "mtn.jack", input$mtn.jack, "\n",
+                "mtn.across", input$mtn.across, "\n",
+                "mtn.in", input$mtn.in, "\n",
+                "mtn.out", input$mtn.out, "\n",
+                "mtn.ignore.jacks", input$mtn.ignore.jacks, "\n",
+                "jck.pow", input$jck.pow, "\n",
+                "mtn.pow", input$mtn.pow, "\n",
+                "dns.pow", input$dns.pow, "\n",
+                "keyset.select", input$keyset.select, "\n",
+                "rate", input$rate, "\n",
+                "smoothing", input$smoothing, "\n",
+                "dif.quant", input$dif.quant, "\n",
+                "decay.ms", input$decay.ms, "\n",
+                "sim.bin.size", input$sim.bin.size,
+                sep = "; "
+            )[1]
+        )
+        
     })
 
     
